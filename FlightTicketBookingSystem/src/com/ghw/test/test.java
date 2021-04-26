2e5dce5c7abded0b2f852519af3c7308ea68aabd
fengye1231 <779419302@qq.com>
2021-04-26 22:35:08 +0800

    2021/4/26

--- FlightTicketBookingSystem/src/com/ghw/test/test.java
+++
@@ -1,7 +0,0 @@
-package com.ghw.test;
-import com.ghw.frame.*;
-public class test {
-	public static void main(String[] agrs){
-		new MainFrame().setVisible(true);
-	}
-}
--- FlightTicketBookingSystem/src/com/ghw/util/FillObject.java
+++
@@ -1,7 +0,0 @@
-package com.ghw.util;
-
-import java.sql.ResultSet;
-
-public interface FillObject {
-	public abstract Object fillObject(ResultSet rs);
-}
