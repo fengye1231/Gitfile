f809a97d63cfb11bbad6b0af8ff6e109a00d2903
fengye1231 <779419302@qq.com>
2021-04-27 12:33:46 +0800

    2021/4/27 12:33

--- FlightTicketBookingSystem/shuju.sql
+++ FlightTicketBookingSystem/shuju.sql
@@ -4,5 +4,19 @@ alter table Flight add flightTime float not null;
 */
 
 use FlightTicketBookingSystem;
+/*
 
+INSERT INTO  City VALUES (11,'武汉');
+INSERT INTO  City VALUES (12,'广州');
+INSERT INTO  City VALUES (13,'香港');
+INSERT INTO  City VALUES (14,'深圳');
+INSERT INTO  City VALUES (15,'北京');
+INSERT INTO  City VALUES (16,'张家口');
+INSERT INTO  City VALUES (17,'石家庄');
+
+INSERT INTO  City VALUES (18,'四川');
+
+*/
+
+INSERT INTO  Flight VALUES (1,1,2);
 
